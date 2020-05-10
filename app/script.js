981298904aa983d42d325b0b75cfb7d01a35f7ce
Trollmite <you@example.com>
2020-05-10 21:17:28 +0300

    swiper review kinda work

--- app/script.js
+++ app/script.js
@@ -1,3 +1,16 @@
 "use strict"
 
 console.log("shits works");
+
+
+var mySwiper = new Swiper ('.swiper-container', {
+    // Optional parameters
+    loop: true,
+    slidesPerView: 3,
+    slidesPerGroup: 3,
+    spaceBetween: 119,
+
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  })
