1799c4e5040fb8893b4fadfa6303b6b22725fb0b
Trollmite <you@example.com>
2020-05-12 22:16:25 +0300

    aboot feature

--- app/script.js
+++ app/script.js
@@ -14,3 +14,37 @@ var mySwiper = new Swiper ('.swiper-container', {
       el: '.swiper-pagination',
     },
   })
+
+
+  // Features slides
+
+let navBlocks = document.querySelectorAll('.nav-block');
+let featureBlocks = document.querySelectorAll('.feature-block');
+
+for (let i = 0; i < navBlocks.length; i++) {
+    navBlocks[i].addEventListener('click', function() {
+        
+        for (let z = 0; z < navBlocks.length; z++) {
+            navBlocks[z].classList.remove('active');
+        }
+        
+        this.classList.add('active');
+
+
+        // Get data-target value
+        let targetSlide = this.dataset.target; // 1, 2, 3
+
+        // featureSwiper.slideTo(targetSlide - 1);
+
+        // Select feature-block elements by data-slide value
+        let slide = document.querySelector('.feature-block[data-slide="'+ targetSlide +'"]');
+
+
+        for (let x = 0; x < featureBlocks.length; x++) {
+            featureBlocks[x].classList.remove('active');
+        }
+
+        slide.classList.add('active');
+        
+    });
+}
